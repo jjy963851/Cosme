38999d47017575827c2a54a08401a3cf052fa6c6
unknown <jjy963851@gmail.com>
2023-03-08 22:02:15 -0500

    First Sample

--- pages/card.js
+++ pages/card.js
@@ -4,13 +4,13 @@ import {Keyboard, Pagination, Navigation} from "swiper";
 import 'swiper/css';
 import { ArrowRightIcon, EyeIcon, ChatIcon} from "@heroicons/react/outline"
 export default function Card(props){
-    const post = props.post
+    {/*const post = props.post*/}
 
     return(
         <div>    
-           {post.steps.map((post)=>(
+           {/*{post.steps.map((post)=>(
               <CardProps key={post.id} productTitle ={post.step_type} ProductImage = {post} ProductName ={post.product_name} BrandName = {post.brand_name} ProductDetail = {post.description}/>
-          ))}
+           ))}*/}
           
         </div>
     );
